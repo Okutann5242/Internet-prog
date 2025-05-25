05e8e5d07349b3645b060c750f1a91605a8d2d4c
Okutann5242 <okunanlar508@gmail.com>
2025-05-25 18:56:36 +0300

    Update app.py

--- final_projesi/app.py
+++ final_projesi/app.py
@@ -362,7 +362,5 @@ def edit(id):
 
 __all__ = ['db', 'User', 'Talep', 'app']
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=int(os.environ.get("PORT",5000)))
